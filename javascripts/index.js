c2e80b7e2460e10ba45a33fc6c4cbd777e316f1d
Bestian Tang <besitan@gmail.com>
2014-11-03 15:23:09 +0800

    img test

--- javascripts/index.js
+++ javascripts/index.js
@@ -5,7 +5,11 @@
     $scope.goban = $goban;
     $scope.goban.data = $dummy;
     $scope.goban.load($goban.myI);
-    $scope.icons = ['http://static.jsbin.com/images/dave.min.svg', 'https://assets-cdn.github.com/images/modules/logos_page/GitHub-Mark.png', 'http://modernweb.com/wp-content/uploads/2014/01/6_reasons_sass_header.jpg', 'http://www.w3schools.com/angular/pic_angular.jpg'];
+    $scope.icons = 
+    ['http://static.jsbin.com/images/dave.min.svg',
+     'https://assets-cdn.github.com/images/modules/logos_page/GitHub-Mark.png',
+     'http://modernweb.com/wp-content/uploads/2014/01/6_reasons_sass_header.jpg',
+     'images/angularJS.jpeg'];
     $scope.navHeight = 50;
     $scope.countHeight = function(){
       return $window.innerHeight - 40;
